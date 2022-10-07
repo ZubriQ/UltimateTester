c3c2b5135819968fb947843134bf3d01a42b86a3
ZubriQ <zubrich157@gmail.com>
2022-10-07 22:04:12 +0700

    ANTLR4 added.

---
+++ UltimateTester/CodeParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using CodeParser.ANTLR4;
+
+// Run test.
+var filePath = "ANTLR4\\CSharpTest1.txt";
+var fileContent = File.ReadAllText(filePath);
+Console.WriteLine(fileContent);
+
+//var inputStream = new AntlrInputStream(fileContent);
+//var cSharpLexer = new CSharpLexer(inputStream);
+//var commonTokenStream = new CommonTokenStream(simpleLexer);
+//var cSharpParser = new CSharpParser(commonTokenStream);
+
+//var programContext = cSharpParser.program();
+//var visitor = new CSharpVisitor();
+//visitor.Visit(programContext);
