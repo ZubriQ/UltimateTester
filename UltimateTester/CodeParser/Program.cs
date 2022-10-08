8131a6609ab95aabc894dabbeebf1e33edf14de2
Ivan Blok <blockin90@gmail.com>
2022-10-08 22:03:48 +0700

    Подключена грамматика https://github.com/antlr/grammars-v4/tree/master/csharp

--- UltimateTester/CodeParser/Program.cs
+++ UltimateTester/CodeParser/Program.cs
@@ -1,15 +1,19 @@
-﻿using CodeParser.ANTLR4;
+﻿using Antlr4.Runtime;
+using CodeParser.ANTLR4;
 
 // Run test.
 var filePath = "ANTLR4\\CSharpTest1.txt";
 var fileContent = File.ReadAllText(filePath);
 Console.WriteLine(fileContent);
 
-//var inputStream = new AntlrInputStream(fileContent);
-//var cSharpLexer = new CSharpLexer(inputStream);
-//var commonTokenStream = new CommonTokenStream(simpleLexer);
-//var cSharpParser = new CSharpParser(commonTokenStream);
-
+var inputStream = new AntlrInputStream(fileContent);
+var cSharpLexer = new CSharpLexer(inputStream);
+var commonTokenStream = new CommonTokenStream(cSharpLexer);
+var cSharpParser = new CSharpParser(commonTokenStream);
+var tokens = cSharpLexer.GetAllTokens();
+foreach(var token in tokens) {
+    Console.Write($"{token.Type} {token.Text}");
+}
 //var programContext = cSharpParser.program();
 //var visitor = new CSharpVisitor();
 //visitor.Visit(programContext);
