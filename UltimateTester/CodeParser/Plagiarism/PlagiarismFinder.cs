3b307fdca32242d12a3acaec66f01954cfa4ac8b
ZubriQ <zubrich157@gmail.com>
2022-10-28 00:32:00 +0700

    Implemented PlagiarismChecker class to compare codes in percentage.

--- UltimateTester/CodeParser/Plagiarism/PlagiarismFinder.cs
+++ UltimateTester/CodeParser/Plagiarism/PlagiarismFinder.cs
@@ -5,7 +5,7 @@ namespace CodeParser.Plagiarism
 {
     public class PlagiarismFinder
     {
-        public NGrammer<string, StringSet> CodeFinder { get; private set; } 
+        private NGrammer<string, StringSet> CodeFinder { get; set; } 
             
         public PlagiarismFinder()
         {
@@ -21,8 +21,7 @@ namespace CodeParser.Plagiarism
         /// <returns>Returns id of the most suitable code string.</returns>
         public string FindMostSuitableCode(string codeToFind)
         {
-            string identifier = CodeFinder.Predict(codeToFind);
-            return identifier;
+            return CodeFinder.Predict(codeToFind);
         }
     }
 }
--- UltimateTester/CodeParser/Program.cs
+++ UltimateTester/CodeParser/Program.cs
@@ -1,35 +1,37 @@
 ﻿using Antlr4.Runtime;
 using CodeParser;
 using CodeParser.ANTLR4;
+using CodeParser.Plagiarism;
 using CodeParser.ZhangShasha;
+using F23.StringSimilarity;
 using System.Diagnostics;
 
-Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
+//Trace.Listeners.Add(new TextWriterTraceListener(Console.Out));
 
-Tree tree1 = SetUpTestTree("ANTLR4\\CSharpTest2.txt");
-Tree tree2 = SetUpTestTree("ANTLR4\\CSharpTest3.txt");
+//Tree tree1 = SetUpTestTree("ANTLR4\\CSharpTest2.txt");
+//Tree tree2 = SetUpTestTree("ANTLR4\\CSharpTest3.txt");
 
-var (distance, list) = Tree.ZhangShasha(tree1, tree2);
-OutputResult(distance);
+//var (distance, list) = Tree.ZhangShasha(tree1, tree2);
+//OutputResult(distance);
 
-Tree SetUpTestTree(string codeFilePath)
-{
-    Trace.WriteLine("\t[Input 1]:");
-    var fileContent = File.ReadAllText(codeFilePath);
-    Trace.WriteLine(fileContent + "\n");
-    Tree tree1 = TreeFactory.ConstructTreeFromString(fileContent);
-    return tree1;
-}
+//Tree SetUpTestTree(string codeFilePath)
+//{
+//    Trace.WriteLine("\t[Input 1]:");
+//    var fileContent = File.ReadAllText(codeFilePath);
+//    Trace.WriteLine(fileContent + "\n");
+//    Tree tree1 = TreeFactory.ConstructTreeFromString(fileContent);
+//    return tree1;
+//}
 
-void OutputResult(int distance)
-{
-    Trace.WriteLine("\n\t[Output]:");
-    Trace.WriteLine("Distance = " + distance);
-    foreach (var o in list)
-    {
-        Trace.WriteLine(o.O.ToString() + " " + o.N1 + " => " + o.N2);
-    }
-}
+//void OutputResult(int distance)
+//{
+//    Trace.WriteLine("\n\t[Output]:");
+//    Trace.WriteLine("Distance = " + distance);
+//    foreach (var o in list)
+//    {
+//        Trace.WriteLine(o.O.ToString() + " " + o.N1 + " => " + o.N2);
+//    }
+//}
 
 // Для Токенов
 //var cSharpCode = File.ReadAllText("TestCode\\CSharpTest6.txt");
