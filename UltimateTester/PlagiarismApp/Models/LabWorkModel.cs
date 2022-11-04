98900ea635d3a8092e23b6ec03931272229b77c1
ZubriQ <zubrich157@gmail.com>
2022-11-04 18:17:26 +0700

    Implemented Add/Edit lab work. Added cancel buttons.

--- UltimateTester/PlagiarismApp/Models/LabWorkModel.cs
+++ UltimateTester/PlagiarismApp/Models/LabWorkModel.cs
@@ -8,7 +8,7 @@ namespace PlagiarismApp.Models
         [Required, MaxLength(50)]
         public string Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         public LabWorkModel()
         {
